93ac103b66c8c0ea5d2a8363ecb568ecd4af043a
HasaniWedagedara <hasaniwedagedara123@gmail.com>
2024-05-09 15:52:57 +0530

    completed the crud operation

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080
 const schemaData= mongoose.Schema({
   name:String,
   email:String,
-  mobile:Number
+  mobile:String,
 },{
   timeStamps:true
 })
@@ -41,7 +41,7 @@ app.post("/create",async(req,res)=>{
   console.log(req.body)
   const data= new userModel(req.body)
   await data.save()
-  res.send({success:true,message:"Data save Successfully",data:data})
+  res.send({success:true,message:"Data saved Successfully",data:data})
 })
 
 //update data method
@@ -55,15 +55,15 @@ app.post("/create",async(req,res)=>{
 }*/
 app.put("/update",async(req,res)=>{
   console.log(req.body)
-  const {id,...rest}=req.body
+  const {_id,...rest}=req.body
   console.log(rest)
-  const data= await userModel.updateOne({_id:id},rest)
+  const data= await userModel.updateOne({_id:_id},rest)
   res.send({success:true,message:"Data updated Successfully",data:data})
 })
 
 //delete method
 //http://localhost:8080/delete
-app.delete("/delete",async(req,res)=>{
+app.delete("/delete/:id",async(req,res)=>{
 const id=req.params.id
 console.log(id)
 const data= await userModel.deleteOne({_id:id})
